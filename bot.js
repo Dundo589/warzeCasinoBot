116417ad9d94919ff17a25c3f1bdee455daaa9e5
Dundo <164530130+Dundo589@users.noreply.github.com>
2024-03-22 16:29:01 +0100

    Only start bot a single time

--- bot.js
+++ bot.js
@@ -1,15 +1,4 @@
 const cluster = require('cluster');
 const { startBot } = require('./start');
-if (cluster.isMaster) {
-    cluster.fork();
 
-    cluster.on('exit', function(worker, code, signal) {
-        console.log("Bot restarting!")
-
-        cluster.fork();
-    });
-}
-
-if (cluster.isWorker) {
-    startBot();
-}
+startBot();
